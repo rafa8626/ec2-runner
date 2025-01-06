6bc9495d1fb7b128bd8d3908f5f8668277e726df
Rafael Miranda <rafa8626@gmail.com>
2025-01-05 20:29:49 -0500

    Removed unnecessary user when installing Docker to avoid errors

--- InstallTools.sh
+++ InstallTools.sh
@@ -16,7 +16,6 @@ sudo apt install -y nodejs
 # Installing Docker 
 sudo apt update
 sudo apt install docker.io -y
-sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 sudo chmod 777 /var/run/docker.sock
